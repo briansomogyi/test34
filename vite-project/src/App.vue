57bb89aa70c78b2ef6f2c87dc98d46e1ed6b143e
briansomogyi <brian.somogyi@emanuel.ro>
2025-01-27 16:52:34 +0200

    Initial commit

---
+++ vite-project/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="flex min-h-screen">
+    <Sidebar />
+    <div class="flex-1 flex flex-col">
+      <Header />
+      <MainContent />
+    </div>
+  </div>
+</template>
+
+<script>
+import Header from './components/Header.vue'
+import Sidebar from './components/Sidebar.vue'
+import MainContent from './components/MainContent.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Header,
+    Sidebar,
+    MainContent,
+  },
+}
+</script>
+
+<style>
+@tailwind base;
+@tailwind components;
+@tailwind utilities;
+</style>
---
+++ vite-project/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{vue,js}', '.src/App.vue', '.src/main.js'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
